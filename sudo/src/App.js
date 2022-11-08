e39d3a26f42a6a94bc189457a8f520e2fa7b5679
ashley <ashley.oketo@student.moringaschool.com>
2022-11-08 22:04:57 +0300

    created different components

--- sudo/src/App.js
+++ sudo/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Home from './components/Home';
+// import Products from './components/Products';
+import {Routes,Route} from 'react-router-dom';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return ( 
+    <>
+    <Navbar/>
+    <Routes>
+    <Route path="/" element={<Home/>}/>
+    {/* <Route path="/products" element={<Products/>}/> */}
+    {/* <Route exact path="/about" component={About}/>
+    <Route exact path="/contact" component={Contact}/> */}
+    </Routes>
+    </>
+  )
 }
 
 export default App;
